bb7c5732ff9955cbf768a236b870d526e945beb3
cyssave <cyssave@gmail.com>
2025-09-26 23:38:24 -0700

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+PORT=3000
+//ALLOWED_ORIGINS=chrome-extension://<YOUR_EXTENSION_ID>,https://www.twitch.tv,https://extension.twitch.tv
+
+// Patreon (only if you want upgrade button working now)
+CLIENT_ID=
+CLIENT_SECRET=
+//REDIRECT_URI=https://<your-railway>.up.railway.app/oauth-callback
+PATREON_CAMPAIGN_ID=
+JWT_SECRET=change_me_to_a_long_random_string
+
+// Twitch (at least one pair)
+//CLIENT_ID_1=
+//CLIENT_SECRET_1=
